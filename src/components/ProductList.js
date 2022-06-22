11a12b037a6c3a576bfffe67bdba6e8abf32769a
cerensolpan <cerensolpan@gmail.com>
2022-06-23 00:05:38 +0300

    feat: Product list is created and useDeferredValue is added

---
+++ src/components/ProductList.js
@@ -0,0 +1,15 @@
+// import {useDeferredValue} from "react";
+
+function ProductList({ products }) {
+  // const deferredProducts = useDeferredValue(products)
+
+  return (
+    <ul>
+         {products.map((product) => (
+        <li>{product}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default ProductList;
